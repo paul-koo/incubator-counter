a8dafa92811112f9523989e3042b918290cea6df
paul-koo <pavelkoltun999@gmail.com>
2024-09-07 12:50:50 +0300

    feat: add logic for counter

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,19 +1,17 @@
-import { colors } from '@mui/material';
-
 export const ButtonStyles = {
     fontWeight: '600',
     borderWidth: '2px',
 };
 
-export const TextFieldStyles = {
-    input: {
-        color: '#1976d2',
-        fontWeight: '600',
-        // border: '2px solid #1976d2',
-    },
-    label: {
-        color: '#1976d2',
-    },
+export const TextFieldStylesFunc = (err?: boolean) => {
+    return {
+        input: {
+            color: err ? 'red' : '#1976d2',
+            fontWeight: '600',
+            // border: '2px solid #1976d2',
+        },
+        label: {
+            color: err ? 'red' : '#1976d2',
+        },
+    };
 };
-
-console.log(colors);
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Button, TextField } from '@mui/material';
-import { ButtonStyles, TextFieldStyles } from './App.styled';
+import { ButtonStyles, TextFieldStylesFunc } from './App.styled';
 
 function App() {
-    const [counter, setCouter] = useState(0);
-    const [maxValue, setMaxValue] = useState<string | undefined>();
+    const [maxValue, setMaxValue] = useState(
+        Number(
+            localStorage.getItem('maxValue')
+                ? localStorage.getItem('maxValue')
+                : 0
+        )
+    );
+    const [startValue, setStartValue] = useState(
+        Number(
+            localStorage.getItem('startValue')
+                ? localStorage.getItem('startValue')
+                : 0
+        )
+    );
+    const [counter, setCouter] = useState(startValue);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        if (maxValue > startValue) setError(false);
+        if (maxValue <= startValue) setError(true);
+        if (maxValue < 0 || startValue < 0) setError(true);
+    }, [maxValue, startValue]);
 
     return (
         <div className="Wrapper">
@@ -17,13 +37,14 @@ function App() {
                         variant="outlined"
                         type="number"
                         size="small"
-                        sx={TextFieldStyles}
+                        sx={TextFieldStylesFunc(error)}
                         value={maxValue}
                         onChange={(
                             event: React.ChangeEvent<HTMLInputElement>
                         ) => {
-                            setMaxValue(event.currentTarget.value);
+                            setMaxValue(+event.currentTarget.value);
                         }}
+                        color={error ? 'error' : 'primary'}
                     />
                     <TextField
                         id="standard-basic"
@@ -31,12 +52,31 @@ function App() {
                         variant="outlined"
                         type="number"
                         size="small"
-                        sx={TextFieldStyles}
-                        color="primary"
+                        sx={TextFieldStylesFunc(error)}
+                        value={startValue}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => {
+                            setStartValue(+event.currentTarget.value);
+                        }}
+                        color={error ? 'error' : 'primary'}
                     />
                 </div>
                 <div className="Button_board">
-                    <Button variant="outlined" size="small" sx={ButtonStyles}>
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        onClick={() => {
+                            localStorage.setItem('maxValue', String(maxValue));
+                            localStorage.setItem(
+                                'startValue',
+                                String(startValue)
+                            );
+                            setCouter(startValue);
+                        }}
+                        sx={ButtonStyles}
+                        disabled={error}
+                    >
                         SET
                     </Button>
                 </div>
@@ -51,6 +91,11 @@ function App() {
                             setCouter(counter + 1);
                         }}
                         sx={ButtonStyles}
+                        disabled={
+                            counter >= Number(localStorage.getItem('maxValue'))
+                                ? true
+                                : false
+                        }
                     >
                         INC
                     </Button>
@@ -58,7 +103,7 @@ function App() {
                         variant="outlined"
                         size="small"
                         onClick={() => {
-                            setCouter(0);
+                            setCouter(startValue);
                         }}
                         sx={ButtonStyles}
                     >
