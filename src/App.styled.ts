99f4c20e02afa758af485fe68f13c748afb335d9
paul-koo <pavelkoltun999@gmail.com>
2024-09-07 09:57:21 +0300

    feat: two board with css style

---
+++ src/App.styled.ts
@@ -0,0 +1,19 @@
+import { colors } from '@mui/material';
+
+export const ButtonStyles = {
+    fontWeight: '600',
+    borderWidth: '2px',
+};
+
+export const TextFieldStyles = {
+    input: {
+        color: '#1976d2',
+        fontWeight: '600',
+        // border: '2px solid #1976d2',
+    },
+    label: {
+        color: '#1976d2',
+    },
+};
+
+console.log(colors);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+import { ButtonStyles, TextFieldStyles } from './App.styled';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCouter] = useState(0);
+    const [maxValue, setMaxValue] = useState<string | undefined>();
+
+    return (
+        <div className="Wrapper">
+            <section className="Board">
+                <div className="Info_board">
+                    <TextField
+                        id="standard-basic"
+                        label="max value"
+                        variant="outlined"
+                        type="number"
+                        size="small"
+                        sx={TextFieldStyles}
+                        value={maxValue}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => {
+                            setMaxValue(event.currentTarget.value);
+                        }}
+                    />
+                    <TextField
+                        id="standard-basic"
+                        label="start value"
+                        variant="outlined"
+                        type="number"
+                        size="small"
+                        sx={TextFieldStyles}
+                        color="primary"
+                    />
+                </div>
+                <div className="Button_board">
+                    <Button variant="outlined" size="small" sx={ButtonStyles}>
+                        SET
+                    </Button>
+                </div>
+            </section>
+            <section className="Board">
+                <div className="Info_board">{counter}</div>
+                <div className="Button_board">
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        onClick={() => {
+                            setCouter(counter + 1);
+                        }}
+                        sx={ButtonStyles}
+                    >
+                        INC
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        onClick={() => {
+                            setCouter(0);
+                        }}
+                        sx={ButtonStyles}
+                    >
+                        RESET
+                    </Button>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
